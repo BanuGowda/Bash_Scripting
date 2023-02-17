5577a781f89ae40cb9767d1d516894a13a5de282
Banu K P <banubharathgowda18@gmail.com>
2023-02-17 16:41:25 +0530

    adding the latest update

---
+++ Filesystem.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+date=`date +"%y-%m-%d_%t"| tr ':' '_'`
+Filename=/Documents/Bash/Filename_$Today.txt
+Pathname=/Documents/Bash/
+
+Result=`df -h | awk '0+$5>=1' | wc | awk '{print $1}'`
+
+if [ $Result -ge 1 ]
+
+then
+
+echo "statictics collected at: $(date) ">>Filename
+
+echo"\n--------------------------------">>Filename
+
+df -h |awk '0+$5 >1 {print $1 "Filesystem has "$5" occupied " }'>>Filename
+
+else
+
+echo "No match found"
+
+fi
+
+
